6b76370cdb2a95a92f3951d7982c77121d3d7b2b
Mitsuo Linus Fujisawa <linusfujisawa@gmail.com>
2026-06-13 09:09:52 -0700

    screenshots and github polish

---
+++ .wrangler/tmp/pages-woLxOY/functionsRoutes-0.274980591540945.mjs
@@ -0,0 +1,19 @@
+import { onRequestPost as __api_create_checkout_js_onRequestPost } from "/Users/linusfujisawa/Desktop/Programming/SmartSenior/MemorialTourSystemV2/functions/api/create-checkout.js"
+import { onRequestGet as __api_verify_session_js_onRequestGet } from "/Users/linusfujisawa/Desktop/Programming/SmartSenior/MemorialTourSystemV2/functions/api/verify-session.js"
+
+export const routes = [
+    {
+      routePath: "/api/create-checkout",
+      mountPath: "/api",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_create_checkout_js_onRequestPost],
+    },
+  {
+      routePath: "/api/verify-session",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_verify_session_js_onRequestGet],
+    },
+  ]
